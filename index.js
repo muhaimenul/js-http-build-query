652c5d9f5172616cb26bc5882a3386964c947e41
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-12-09 21:23:07 +0600

    parameter check

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = (data) => {
-    if (typeof (data) === 'string') return data;
+    if (typeof (data) === 'string' || typeof (data) === 'undefined' || typeof (data) === 'object') return data
     
-    let query = [];
+    let query = []
     
     for (let param in data) {
         if (data.hasOwnProperty(param)) {
-            query.push(encodeURI(param + '=' + data[param]));
+            query.push(encodeURI(param + '=' + data[param]))
         }
     }
 
-    return query.join('&');
+    return query.join('&')
 }
