f463214f66d27e6f66383760285c17dbf8475461
muhaimenul <muhaimenulislam@iut-dhaka.edu>
2022-05-24 06:43:59 +0600

    improved query builder to support nested object parameters

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-module.exports = (data) => {
+module.exports = (data, prefix) => {
     if (typeof (data) === 'undefined' || typeof (data) !== 'object') return data
     
     let query = []
-    
+
     for (let param in data) {
         if (data.hasOwnProperty(param)) {
-            query.push(encodeURI(param + '=' + data[param]))
+            let key = prefix ? prefix + "[" + param + "]" : param,
+                value = data[param];
+                query.push(typeof v == "object" ? serialize(value, key) : encodeURIComponent(key) + "=" + encodeURIComponent(value));
         }
     }
 
     return query.join('&')
+
 }
